754b60968cdc9d3347130e35775ab6fda74bd22f
ClaytonDewey <clay@dryadmedia.com>
2023-06-19 15:30:08 -0500

    Create the TenureSelect component

---
+++ src/Components/TenureSelect.js
@@ -0,0 +1,27 @@
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+
+const TenureSelect = ({ data, setData }) => {
+  const handleChange = (event) => {
+    setData({ ...data, loanTerm: event.target.value });
+  };
+
+  return (
+    <FormControl fullWidth>
+      <InputLabel id='demo-simple-select-label'>Tenure</InputLabel>
+      <Select
+        labelId='demo-simple-select-label'
+        id='demo-simple-select'
+        valud={data.loanTerm}
+        label='Tenure'
+        defaultValue={5}
+        onChange={handleChange}>
+        <MenuItem value={5}>5 years</MenuItem>
+        <MenuItem value={10}>10 years</MenuItem>
+        <MenuItem value={15}>15 years</MenuItem>
+        <MenuItem value={20}>20 years</MenuItem>
+        <MenuItem value={25}>25 years</MenuItem>
+      </Select>
+    </FormControl>
+  );
+};
+export default TenureSelect;
